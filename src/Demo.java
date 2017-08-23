2e8fd136dfcb5ace02aee58476bab970e201d987
MonikaDhanorkar <30587082+MonikaDhanorkar@users.noreply.github.com>
2017-08-23 15:43:55 +0530

    third commit

--- src/Demo.java
+++ src/Demo.java
@@ -2,12 +2,6 @@
 public class Demo {
 
 	public static void main(String[] args) {
-           for(int i=0;i<10;i++)
-			
-		{
-			System.out.println("hello world..."+i);
-		}
-
            
            char arr[] = {'a','b','c','d'};
            System.out.println(arr[78]);
