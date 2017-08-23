0aa3d92c09b0ef67419ceebc4ad479dfd395a7b4
MonikaDhanorkar <30587082+MonikaDhanorkar@users.noreply.github.com>
2017-08-23 16:41:12 +0530

    fifth commit

--- src/Demo.java
+++ src/Demo.java
@@ -4,8 +4,9 @@ public class Demo {
 	public static void main(String[] args) {
            
            char arr[] = {'a','b','c','d'};
+           System.out.println(arr[3]); 
            System.out.println(arr[78]);
-        System.out.println(arr[3]); 
+      
 	}
 
 }
