0a1aa0cce7612bd704ff922d678a5771620bf2f5
MonikaDhanorkar <30587082+MonikaDhanorkar@users.noreply.github.com>
2017-08-23 15:35:38 +0530

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,19 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+           for(int i=0;i<10;i++)
+			
+		{
+			System.out.println("hello world..."+i);
+		}
+
+           
+           char arr[] = {'a','b','c','d'};
+           System.out.println(arr[78]);
+           
+           
+           System.out.println("new line added");
+	}
+
+}
